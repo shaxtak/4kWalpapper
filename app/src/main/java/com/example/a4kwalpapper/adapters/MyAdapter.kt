8a240fe6ad962d12b88046720d461f1e4e389b80
shaxtak <150793544+shaxtak@users.noreply.github.com>
2024-01-11 15:40:05 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a4kwalpapper/adapters/MyAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.a4kwalpapper.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a4kwalpapper.R
+
+class MyAdapter(private val dataList: List<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.itemrv, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = dataList[position]
+        holder.bindData(data)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bindData(data: String) {
+            textView.text = data
+        }
+    }
+}
